f2dfd1659c7247c01ecd7e7e2499e345de7d54b7
Chen Yu <scheduledx@gmail.com>
2017-08-11 03:21:54 +0800

    Satellite and post processing

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // === Public Variables ====
+
+
+    // === Private Variables ====
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1f * Time.deltaTime, 0, -1f * Time.deltaTime, Space.World);
+    }
+}
